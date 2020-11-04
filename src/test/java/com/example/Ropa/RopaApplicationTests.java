34d84d9856fdfbd00531f99bddee85328f1d938b
cesargusa22 <cesargusa22@gmail.com>
2020-11-04 17:05:18 +0100

    First Commit

---
+++ src/test/java/com/example/Ropa/RopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ropa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
