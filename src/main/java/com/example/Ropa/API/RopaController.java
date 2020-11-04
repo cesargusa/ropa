cddb6dd5b509b63433ab59dea2b8c51c23ded4c5
cesargusa22 <cesargusa22@gmail.com>
2020-11-04 17:43:58 +0100

    Metodo Post añadido y funcionando

--- src/main/java/com/example/Ropa/API/RopaController.java
+++ src/main/java/com/example/Ropa/API/RopaController.java
@@ -9,6 +9,8 @@ import com.example.Ropa.Model.Ropa;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -39,5 +41,12 @@ public class RopaController {
                      return element;
             }
             return null;
+        }
+
+
+        @PostMapping("/Ropas")
+        public Ropa Add(@RequestBody Ropa NewRopa){
+            ropas.add(NewRopa);
+            return NewRopa;
         }
 }
